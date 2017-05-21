fd8cc317e3c633def5c66303619f9571e62acb07
wisleans <wisleans@gmail.com>
2017-05-20 21:55:00 -0700

    day 11

--- cppHackerRank/cppHackerRank.cpp
+++ cppHackerRank/cppHackerRank.cpp
@@ -306,11 +306,36 @@ int main()
 #pragma endregion DAY 9 Factorial
 
 #pragma region DAY 10 Binary
-
+/*
 int main() {
 	int n;
 	cin >> n;
 	return 0;
 }
+*/
+#pragma endregion DAY 10 Binary
 
-#pragma endregion DAY 10
+#pragma region DAY 11 2D*
+/*
+int main() {
+	int maximum = INT16_MIN;
+	vector< vector<int> > arr(6, vector<int>(6));
+	for (int arr_i = 0;arr_i < 6;arr_i++) {
+		for (int arr_j = 0;arr_j < 6;arr_j++) {
+			cin >> arr[arr_i][arr_j];
+		}
+	}
+	for (int i = 0;i < 4;i++) {
+		for (int j = 0;j < 4;j++) {
+			maximum = max(maximum,
+				arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+				+ arr[i + 1][j + 1]
+				+ arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
+		}
+	}
+	cout << maximum << endl;
+
+	return 0;
+}
+*/
+#pragma endregion DAY 11 2D
